44fc625fa2922cca85ccb6bec337a09280b405aa
MichaelFunes <funesmichael213@gmail.com>
2022-05-31 19:56:48 -0400

    2nd commit

--- src/giHub/gitAndGithub.java
+++ src/giHub/gitAndGithub.java
@@ -3,8 +3,19 @@ package giHub;
 public class gitAndGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.print("today we learn github");
+	System.out.println("today is a very good day");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
